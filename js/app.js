bd34a96b9478335fd5a05f1640d2041fb90d502a
Young Wook Baek <kywbaek@gmail.com>
2018-01-16 23:15:53 +0000

    fix: convert var to const where needed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-    var app = angular.module('myApp', []);
+    const app = angular.module('myApp', []);
     app.controller('myCtrl', function($scope) {
         $scope.firstName2= "John";
         $scope.lastName2= "Doe";
