4f35c04b52a1ec376c4e6093f8ed9044a289e86f
Young Wook Baek <kywbaek@gmail.com>
2018-01-16 23:26:21 +0000

    feat: add 4. Modules

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
     const app = angular.module('myApp', []);
+
     app.controller('myCtrl', function($scope) {
         $scope.firstName2= "John";
         $scope.lastName2= "Doe";
     });
+
+    app.directive("testDirective", function() {
+    return {
+        template : "I was made in a directive constructor!"
+        };
+    });
+
