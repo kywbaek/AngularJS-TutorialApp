5723f0f269045aa0065193f300a9178c4225a7f0
Young Wook Baek <kywbaek@gmail.com>
2018-01-17 21:37:44 +0000

    feat: add 5. Directives

--- js/app.js
+++ js/app.js
@@ -6,8 +6,30 @@
     });
 
     app.directive("testDirective", function() {
-    return {
-        template : "I was made in a directive constructor!"
-        };
+        return {
+            template : "I was made in a directive constructor!"
+            };
     });
+
+    app.directive("testDirective5", function() {
+        return {
+            restrict: "E",
+            template : "<h4>Made by a custom directive with 'E/Element' restrict!</h4>"
+            };
+    });
+
+    app.directive("testDirective51", function() {
+        return {
+            restrict: "M",
+            replace: true,
+            template : "<h4>Made by a custom directive with 'M/coMment' restrict!</h4>"
+            };
+    });
+
+/*  The legal restrict values are:
+    E for Element name
+    A for Attribute
+    C for Class
+    M for Comment (When replace is false, Angular puts the template content inside the comment. When replace is true, Angular replaces the comment with the template content.)
+    By default the value is EA, meaning that both Element names and attribute names can invoke the directive. */
 
