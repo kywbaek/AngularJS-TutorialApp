36be967f446607f241975c083120a61f022741ba
Young Wook Baek <kywbaek@gmail.com>
2018-01-22 20:58:00 +0000

    feat: add 7-1. ng-click function under controller

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-    const app = angular.module('myApp', []);
+const app = angular.module('myApp', []);
 
-    app.controller('myCtrl', function($scope) {
-        $scope.firstName2= "John";
-        $scope.lastName2= "Doe";
-    });
+app.controller('myCtrl', function($scope) {
+    $scope.firstName2= "John";
+    $scope.lastName2= "Doe";
+});
 
-    app.directive("testDirective", function() {
-        return {
-            template : "I was made in a directive constructor!"
-            };
-    });
+app.directive("testDirective", function() {
+    return {
+        template : "I was made in a directive constructor!"
+        };
+});
 
-    app.directive("testDirective5", function() {
-        return {
-            restrict: "E",
-            template : "<h4>Made by a custom directive with 'E/Element' restrict!</h4>"
-            };
-    });
+app.directive("testDirective5", function() {
+    return {
+        restrict: "E",
+        template : "<h4>Made by a custom directive with 'E/Element' restrict!</h4>"
+        };
+});
 
-    app.directive("testDirective51", function() {
-        return {
-            restrict: "M",
-            replace: true,
-            template : "<h4>Made by a custom directive with 'M/coMment' restrict!</h4>"
-            };
-    });
+app.directive("testDirective51", function() {
+    return {
+        restrict: "M",
+        replace: true,
+        template : "<h4>Made by a custom directive with 'M/coMment' restrict!</h4>"
+        };
+});
 
 /*  The legal restrict values are:
     E for Element name
@@ -33,3 +33,9 @@
     M for Comment (When replace is false, Angular puts the template content inside the comment. When replace is true, Angular replaces the comment with the template content.)
     By default the value is EA, meaning that both Element names and attribute names can invoke the directive. */
 
+app.controller('myCtrl7', function($scope) {
+    $scope.name= "Michael Jordan??";
+    $scope.changeName = function() {
+        $scope.name = "Stephen Curry!!";
+    }
+});
